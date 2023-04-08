59cebd4165c56a9c117995d8454dc9a917858fa1
Bikash Kumar <contactbikash2020@gmail.com>
2023-04-08 11:53:19 +0530

    add link

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -9,7 +9,7 @@ function Card() {
                     <h5 class="card-title">Ios App Devlopment</h5>
 
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="card-link">Read more</a>
+                    <a href="/" class="card-link">Read more</a>
 
                 </div>
             </div></>
--- src/Component/Circle.js
+++ src/Component/Circle.js
@@ -13,8 +13,8 @@ function Circle(value) {
                 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
                     <defs>
                         <linearGradient id="GradientColor">
-                            <stop offset="0%" stop-color="#e91e63" />
-                            <stop offset="100%" stop-color="#673ab7" />
+                            <stop offset="0%" stopColor="#e91e63" />
+                            <stop offset="100%" stopColor="#673ab7" />
                         </linearGradient>
                     </defs>
                     <circle cx="80" cy="80" r="70" stroke-linecap="round" />
